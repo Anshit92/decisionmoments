1448a4fdc36b57633abb964a8e5ef21952adf205
Anshit92 <anshit92@live.com>
2017-02-28 12:20:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,3 +7,13 @@ for f in $( cd $AMI_SBIN; ls ) ; do
 done
 
 sh /home/mapr/sbin/installer-wrapper.sh $1 $2 $3 $4 $5 $6 $7 $8
+function install {
+mkdir -p /opt/nifi
+cd /opt/nifi/
+wget http://redrockdigimark.com/apachemirror/nifi/1.1.2/nifi-1.1.2-bin.tar.gz
+tar -xzvf nifi-1.1.2-bin.tar.gz
+#/opt/nifi/nifi-1.1.2/bin/nifi.sh start
+
+}
+
+install
